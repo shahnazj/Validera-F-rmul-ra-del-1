6b704ad1f7d7ca54841994151431813e421ad3fe
shahnazj <shahnaz.jafari35@gmail.com>
2024-10-26 20:52:42 +0200

    validera

---
+++ script.js
@@ -0,0 +1,18 @@
+
+const regForm = document.querySelector('#regForm');
+const firstName = document.querySelector('#firstName');
+   
+
+  regForm.addEventListener('submit', e => {
+    e.preventDefault
+
+    const user = {
+        firstName: firstName.ariaValueMax
+
+    }
+    e.preventDefault(user)
+
+  })
+
+
+
